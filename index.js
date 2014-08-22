5fe48ae29476ab15b198d1899a85dca1e1233141
Peter <peterhsaxton@gmail.com>
2014-08-22 09:32:11 +0100

    four point initialisation tests

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-function Rectangle(x0, y0, x1, y1) {
+function BBox(x0, y0, x1, y1) {
     this.x0 = x0;
     this.y0 = y0;
     this.x1 = x1;
     this.y1 = y1;
+    this.width = x1 - x0;
+    this.height = y1 - y0;
+    Object.freeze(this);
 }
 
 exports.create = function (x0, y0, x1, y1) {
-    return new Rectangle(x0, y0, x1, y1);
+    return new BBox(x0, y0, x1, y1);
+};
+
+exports.isBBox = function (obj) {
+    return (obj.constructor === BBox)
 }
--- test.js
+++ test.js
@@ -13,5 +13,32 @@ describe('Rectangle', function () {
             expect(box.x1).toEqual(3);
             expect(box.y1).toEqual(4);
         });
+
+        it('should create width and height attribute', function () {
+            var box = BBox.create(1, 2, 3, 4);
+            expect(box.width).toEqual(2);
+            expect(box.height).toEqual(2);
+        });
+
+        it('should be a BBox object', function () {
+            var box = BBox.create(1, 2, 3, 4);
+            expect(BBox.isBBox(box)).toBe(true);
+        });
+
+        it('should not validate plain objects', function () {
+            expect(BBox.isBBox({
+                x0: 1,
+                y0: 2,
+                x1: 3,
+                y1: 4,
+                width: 2,
+                height: 2
+            })).toBe(false);
+        });
+
+        it('should create a frozen object', function () {
+            var box = BBox.create(1, 2, 3, 4);
+            expect(Object.isFrozen(box)).toBe(true);
+        });
     });
 });
